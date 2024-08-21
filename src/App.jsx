94c7af9dec7e2d9266595ed0ce8f09fec78c7256
Angela Aguila <angela.aguila@gmail.com>
2024-08-21 02:28:08 -0400

    funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./component/NavBar.jsx";
-//import Home from "./component/Home.jsx";
+import Home from "./component/Home.jsx";
 // import RegisterPage from "./component/RegisterPage.jsx";
 // import LoginPage from "./component/LoginPage.jsx";
-//import Cart2 from "./component/Cart2.jsx";
-import Cart3 from "./component/Cart3.jsx";
+import Cart from "./component/Cart.jsx";
 import Footer from "./component/Footer.jsx";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      {/* <Home></Home> */}
+      <Home></Home>
       {/* <RegisterPage></RegisterPage> */}
       {/* <LoginPage></LoginPage> */}
-      {/* <Cart2></Cart2> */}
-      <Cart3></Cart3>
+      <Cart></Cart>
       <Footer></Footer>
     </>
   );
--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,80 +1,195 @@
 // Simular un carrito de compras, renderizar en App.jsx y comentar componente <Home /> por ahora en App.jsx.
-
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image';
-import PizzaTrozo from '../assets/imgs/PizzaTrozo.png';
-import carroCompra from '../assets/imgs/carroCompra.png';
+import React, { useState } from 'react';
 import { pizzas } from "../pizzas.js";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { Container } from "react-bootstrap";
-import { useState } from 'react';
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import carroCompra from "../assets/imgs/carroCompra.png";
+import ListGroup from "react-bootstrap/ListGroup";
 
 
 export const Cart = () => {
-
-    const styleCard = {
-      width: "25%",
-      height: "auto",
-      margin: "40px 10px 8px 20px",
+  const styleCard = {
+    width: "23%",
+    height: "auto",
+    margin: "10px 10px 8px 20px",
   };
-  
-  const [cuenta, setCuenta] = useState(0);
-  
+
+  //  genera la lista de pizzas en el carrito
+  const [listaPizzas, setListaPizzas] = useState([]);
+  // es Total de la lista de pizzas a pagar
+  const [total, setTotal] = useState(0);
+  //  Cantidad de pizzas en el carrito, por cada una de las seleccionadas
+  const [cantidadPizzas, setCantidadPizzas] = useState(0);
+
+  // Agrega, suma o resta las pizzas a la lista
+  const agregaPizza = (pizza, operacion) => {
+    if (operacion === "agregar") {
+      const found = listaPizzas.findIndex((Lista) => Lista.id === pizza.id);
+
+      if (found < 0) {
+        // primera carga de cada pizza seleccionada a la lista y total
+        setTotal(total + pizza.price);
+        setListaPizzas([
+          ...listaPizzas,
+          {
+            id: pizza.id,
+            name: pizza.name,
+            price: pizza.price,
+            cantidad: 1,
+          },
+        ]);
+      } else {
+        alert("La pizza ya se encuentra en el carrito de compras");
+      }
+    } else if (operacion === "sumar") {
+      // busca en la lista, el ID que se está agregando
+      const pizzas = listaPizzas.map((pizzaEnLista) =>
+        pizzaEnLista.id === pizza.id
+          ? // si lo encuentra, aumenta la cantidad de la pizza en 1
+            { ...pizzaEnLista, cantidad: pizzaEnLista.cantidad + 1 }
+          : pizzaEnLista
+      );
+
+      // Al Total le suma la pizza seleccionada
+      setTotal(total + pizza.price);
+      return setListaPizzas([...pizzas]);
+    } else if (operacion === "restar") {
+      // Copia de la lista para trabajar en ella y luego actualizar
+      const nuevasPizzas = [...listaPizzas];
+
+      // Busca indice de la pizza para restar la cantidad
+      const index = nuevasPizzas.findIndex((el) => el.id === pizza.id);
+      nuevasPizzas[index].cantidad = pizza.cantidad - 1;
+      console.log("listado original");
+      console.log(nuevasPizzas);
+
+      // Actualizar cantidad en el carrito de compra
+      setListaPizzas(nuevasPizzas);
+      console.log("listado actualizado");
+      console.log(listaPizzas);
+
+      // Actualizar Total a Pagar según Pizza eliminada
+      setTotal(total - pizza.price);
+
+      // Eliminar Pizza con cantidad = 0
+      console.log("cantidad de pizzas");
+      console.log(nuevasPizzas[index].cantidad);
+      if (nuevasPizzas[index].cantidad === 0) {
+        const listaActualizada = listaPizzas.filter(
+          (pizzaNull) => pizzaNull.id !== pizza.id
+        );
+        console.log("lista sin cantidades = 0");
+        console.log(listaActualizada);
+        setListaPizzas(listaActualizada);
+      }
+    }
+  };
+
   return (
     <>
       <Container fluid>
         <Row>
-          <Button variant="outline-info">Total: $</Button>
-        </Row>
+          {/* LISTADO DE PIZZAS A PAGAR */}
+          <Col xs={12} md={2} className="d-flex flex-column">
+            <Form>
+              <Form.Group className="d-flex align-items-center">
+                <Button variant="outline-light" className="text-primary">
+                  Total: $ {total}
+                </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  style={{ width: "50%" }}
+                >
+                  Pagar
+                </Button>
+              </Form.Group>
 
-        <Row xs={1} md={2} className="g-4">
-          {pizzas.map((pizza, id) => (
-            <Card style={styleCard} key={pizza.id}>
-              <Card.Header style={{ height: "auto" }}>
-                <Card.Img
-                  variant="top"
-                  src={pizza.img}
-                  style={{ width: "100%" }}
-                />
-                <Card.Title className="mt-3">Pizza {pizza.name}</Card.Title>
-                <Card.Text>{pizza.desc}</Card.Text>
-              </Card.Header>
+              <Form.Label>
+                <h5>Carrito de Compras</h5>
+              </Form.Label>
 
-              <Card.Body className="text-center">
-                <Card.Text className="h5 mb-2">
-                  <Image src={PizzaTrozo} /> Ingredientes:
-                </Card.Text>
-                <Card.Text>{pizza.ingredients}</Card.Text>
-              </Card.Body>
+              <Form.Text className="mb-3">
+                <ListGroup as="ol" numbered>
+                  {listaPizzas.length ? (
+                    <>
+                      {listaPizzas.map((pizza) => (
+                        <ListGroup.Item
+                          key={pizza.id}
+                          className="d-flex justify-content-between align-items-start"
+                        >
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Pizza {pizza.name}</div>
+                            Precio: ${pizza.price}
+                          </div>
+                          <Button onClick={() => agregaPizza(pizza, "restar")}>
+                            -
+                          </Button>
+                          <Form.Text name="cantidad">
+                            {pizza.cantidad}
+                          </Form.Text>
+                          <Button onClick={() => agregaPizza(pizza, "sumar")}>
+                            +
+                          </Button>
+                        </ListGroup.Item>
+                      ))}
+                    </>
+                  ) : (
+                    <Form.Text> Carro vacío </Form.Text>
+                  )}
+                </ListGroup>
+              </Form.Text>
+            </Form>
+          </Col>
 
-              <Card.Footer className="p-4 text-center">
-                <Card.Text className="h4 mb-4">
-                  Precio: ${Intl.NumberFormat("de-DE").format(pizza.price)}
-                </Card.Text>
+          {/* Listado de pizzas para seleccionar  */}
+          <Col
+            xs={12}
+            md={10}
+            className="d-flex justify-content-start flex-wrap my-5"
+          >
+            {/* Renderizar una pizza por tarjeta */}
+            {pizzas.map((pizza) => (
+              <Card style={styleCard} key={pizza.id}>
+                <Card.Header style={{ height: "auto" }}>
+                  <Card.Img
+                    alt={pizza.name}
+                    variant="top"
+                    src={pizza.img}
+                    style={{ width: "100%" }}
+                  />
+                  <Card.Title className="mt-3">Pizza {pizza.name}</Card.Title>
+                </Card.Header>
+                <form>
+                  <Card.Footer className="p-4 text-center">
+                    <Card.Text className="h4 mb-4">
+                      Precio: ${Intl.NumberFormat("de-DE").format(pizza.price)}
+                    </Card.Text>
 
-                <Card.Text key={pizza.id}>
-                  <Button
-                    onClick= () => setCuenta(cuenta - 1)}
-                    variant="ou|tline-primary"
-                  >
-                    -
-                  </Button>{" "}
-                  {cuenta}{" "}
-                  <Button
-                    onClick={() => setCuenta(cuenta + 1)}
-                    variant="outline-secondary"
-                  >
-                    +
-                  </Button>
-                </Card.Text>
-              </Card.Footer>
-            </Card>
-          ))}
+                    <Button
+                      className="mx-3"
+                      onClick={() => agregaPizza(pizza, "agregar")}
+                    >
+                      <Card.Img
+                        variant="top"
+                        src={carroCompra}
+                        style={{ width: "20%" }}
+                      />
+                      Añadir
+                    </Button>
+                  </Card.Footer>
+                </form>
+              </Card>
+            ))}
+          </Col>
         </Row>
       </Container>
     </>
   );
-}
+};
 
 export default Cart;
